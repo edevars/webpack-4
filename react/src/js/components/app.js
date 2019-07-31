f4664071e970edb6ce5a82e9ac18647c5d81e624
edevars <e.devars.dev@gmail.com>
2019-07-31 15:35:01 -0500

    🥒 Rick and Morty App

---
+++ react/src/js/components/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Home from "./Home";
+
+const App = () => {
+  return (
+    <div className="HomeContainter">
+    <img
+        src="https://blog.blackmilkclothing.com/wp-content/uploads/2019/01/RAM__LOGO_4C_S3SG.png"
+        alt="title"
+        className="imageTitle"
+      />
+      
+      <Home />
+    </div>
+  );
+};
+
+export default App;
