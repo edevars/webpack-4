c2bc2abce89e2602e7668594362753f8c09d8a81
edevars <e.devars.dev@gmail.com>
2019-07-31 15:34:10 -0500

    ⚛ React webpack config

---
+++ react/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/js/index.js"),
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    port: 9000
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: "React ⚛",
+      template: path.resolve(__dirname, "index.html"),
+    })
+  ]
+};
